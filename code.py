1e11e32719f27b05011088bdb9f073a2a16abdb9
AtikshSambhav <78915859+AtikshSambhav@users.noreply.github.com>
2022-08-03 19:52:24 +0530

    Add files via upload
    GUESS THE NUMBER AND ENJOY✨

---
+++ code.py
@@ -0,0 +1,40 @@
+import random
+
+print("Number guessing game")
+
+
+number = random.randint(1, 9)
+
+
+chances = 0
+
+print("Guess a number (between 1 and 9):")
+
+
+while chances < 5:
+
+  
+    guess = int(input("Enter your guess:- "))
+
+   
+
+    if guess == number:
+        print("CONGRATULATIONS YOU HAVE GUESSED IT RIGHT")
+        break
+
+    
+    elif guess < number:
+        print("Guessed Number Is Small,Try A Bigger Number")
+        
+    
+    else:
+        print("The Number Guessed Is Bigger, Try a Small Number ")
+       
+
+   
+    chances += 1
+
+
+
+if not chances < 5:
+    print("YOU LOSE!!! The number is", number)
